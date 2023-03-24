09d68e817aff4ab647e0416399a44bbb0cf201c7
jcraiz <63567373+jcraiz@users.noreply.github.com>
2023-03-24 12:35:49 -0500

    Add files via upload

---
+++ data/slide7.js
@@ -0,0 +1,5 @@
+(function(){var loadHandler=window['sl_{733CDE11-7E66-4F54-B4B0-B4379D041900}'];loadHandler&&loadHandler(6, '<div id="spr0_22c3d27e"><div id="spr1_22c3d27e" class="kern slide"><img id="img4_22c3d27e" src="data/img3.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_22c3d27e"><img id="img0_22c3d27e" src="data/img0.png" width="6" height="540" alt="" style="left:-0.105px;"/></div><div id="spr4_22c3d27e" style="left:954.21px;"><img id="img1_22c3d27e" src="data/img0.png" width="6" height="540" alt="" style="left:-0.105px;"/></div><div id="spr5_22c3d27e" style="left:289.228px;top:469.135px;"><img id="img2_22c3d27e" src="data/img1.png" width="382" height="71" alt="A picture containing drawing\
+\
+Description automatically generated" style="left:-0.228px;top:-0.068px;"/></div><div id="spr6_22c3d27e" style="left:372.325px;top:162.325px;"><img id="img3_22c3d27e" src="data/img2.png" width="215" height="215" alt="A picture containing food\
+\
+Description automatically generated" style="left:0.175px;top:0.175px;"/></div></div><div id="spr2_22c3d27e" class="kern slide"></div></div>', '{"s":[]}');})();
